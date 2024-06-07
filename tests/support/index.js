38284c44c80535679c5a66dc3be41fe06ec422eb
DESKTOP-QGMVF6V\joao_ <joao_marcossilva@hotmail.com>
2024-06-07 17:16:50 -0300

    Update extensão page

--- tests/support/index.js
+++ tests/support/index.js
@@ -1,25 +1,33 @@
 //ESSA IMPLEMENTAÇÃO É EXATAMENTE COMO O page dentro dos arquivos de teste funcionam
-//Porém iremos sobreescrever o page original, onde iremos injetar dentro da classe page
+//Porém iremos criar uma extensão do page original, onde iremos injetar dentro da classe page
 //todos os PageObjects, e nao vou precisar criar eles dentro das classes PageObject
 
-const {test: base,expect} = require('@playwright/test');
+//ORIGINAL PAGE 
+/*const test = base.extend({
+    page: async ({page}, use) =>{
+        await use(page)
+    }
+})*/
 
-const {LoginPage} = require('../pages/LoginPage');
-const {MoviesPage} = require('../pages/MoviesPage')
-const {LandingPage} = require('../pages/LandingPage');
-const {Toast, Alert} = require('../pages/Components');
+const { test: base, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage');
+const { MoviesPage } = require('../pages/MoviesPage')
+const { LandingPage } = require('../pages/LandingPage');
+const { Toast, Alert } = require('../pages/Components');
 
 const test = base.extend({
-    page: async ({page}, use) =>{
-        await use({
-            ...page,
-            landing: new LandingPage(page),
-            login: new LoginPage(page),
-            movies: new MoviesPage(page),
-            toast: new Toast(page),
-            alert: new Alert(page)    
-        })
+    page: async ({ page }, use) => {
+        const context = page;
+
+        context['landing'] = new LandingPage(page);
+        context['login'] = new LoginPage(page);
+        context['movies'] = new MoviesPage(page);
+        context['toast'] = new Toast(page);
+        context['alert'] = new Alert(page);
+
+        await use(context);
     }
 })
 
-export {test,expect};
+
+export { test, expect };
