f04302b3f0df7e63430fa7d48f5e82dda951226f
DESKTOP-QGMVF6V\joao_ <joao_marcossilva@hotmail.com>
2024-07-02 18:21:54 -0300

    Ajustando classes de API

---
+++ tests/api/movie_api.spec.js
@@ -0,0 +1,27 @@
+const { test, expect } = require('../support');
+const data = require('../support/fixtures/movies.json');
+const { executeSQL } = require('../support/database');
+
+test('API - deve cadastrar um filme', async ({page,request})=>{
+    const movie = data.filme.zumbilandia;
+    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`);
+    await request.moviesApi.createMovie(movie);
+})
+
+test('API - nao deve cadastrar um filme que ja existe', async ({ page,request }) => {
+  
+    const movie = data.filme.resident_evil_o_hospedeiro;
+
+    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`);
+    
+    await request.moviesApi.createMovie(movie);
+    await page.login.logar('admin@zombieplus.com', 'pwd123');
+    await page.movie.create(movie);
+    await page.popup.haveText(`O título '${movie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`)
+})
+
+test('API - deve remover um filme', async ({page,request})=>{
+    const movie = data.filme.exterminio;
+    await request.moviesApi.createMovie(movie);
+    await request.moviesApi.deleteMovie(movie);
+})
--- tests/e2e/movie.spec.js
+++ tests/e2e/movie.spec.js
@@ -2,18 +2,6 @@ const { test, expect } = require('../support');
 const data = require('../support/fixtures/movies.json');
 const { executeSQL } = require('../support/database');
 
-test('(sem contexto)deve cadastrar um novo filme', async ({ page }) => {
-
-    /*const movie = data.guerra_mundial_z;
-    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`)
-
-    await loginPage.visit();
-    await loginPage.submit('admin@zombieplus.com','pwd123');
-    await moviesPage.isLoggedIn();
-    await moviesPage.create(movie.title,movie.overview,movie.company,movie.release_year);
-    await toast.haveText('Cadastro realizado com sucesso')*/
-})
-
 test('deve cadastrar um novo filme', async ({ page }) => {
 
     const movie = data.filme.guerra_mundial_z;
@@ -38,18 +26,6 @@ test('Web - nao deve cadastrar um filme que ja existe', async ({ page,request })
     await page.popup.haveText(`O título '${movie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`)
 })
 
-test('API - nao deve cadastrar um filme que ja existe', async ({ page,request }) => {
-  
-    const movie = data.filme.resident_evil_o_hospedeiro;
-
-    await executeSQL(`DELETE from movies WHERE title = '${movie.title}';`);
-
-    await request.moviesApi.createMovie(movie);
-    await page.login.logar('admin@zombieplus.com', 'pwd123');
-    await page.movie.create(movie);
-    await page.popup.haveText(`O título '${movie.title}' já consta em nosso catálogo. Por favor, verifique se há necessidade de atualizações ou correções para este item.`)
-})
-
 test('nao deve permitir criar filme sem preencher campos obrigatórios', async ({page})=>{
     await page.login.logar('admin@zombieplus.com', 'pwd123');
     await page.movie.goForm();
@@ -69,8 +45,3 @@ test('WEB - deve remover um filme', async ({page,request})=>{
     await page.popup.haveText('Filme removido com sucesso.');
 })
 
-test('API - deve remover um filme', async ({page,request})=>{
-    const movie = data.filme.exterminio;
-    await request.moviesApi.createMovie(movie);
-    await request.moviesApi.deleteMovie(movie);
-})
