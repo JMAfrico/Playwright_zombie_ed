f60ea2f60efa0701186226860877d8beebd17a68
DESKTOP-QGMVF6V\joao_ <joao_marcossilva@hotmail.com>
2024-07-25 17:35:26 -0300

    teste

--- tests/e2e/tvshow.spec.js
+++ tests/e2e/tvshow.spec.js
@@ -6,3 +6,10 @@ test('Cadastrar nova série', async({page}) => {
     await page.tvshow.goForm();
     await page.tvshow.setTitle();
 });
+
+test('Não cadastrar série', async({page}) => {
+    await page.login.logar('admin@zombieplus.com', 'pwd123');
+    await page.tvshow.goMenu();
+    await page.tvshow.goForm();
+    await page.tvshow.setTitle();
+});
